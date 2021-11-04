1e2b66351c3155baf54178b6489ba813c5b87376
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2021-11-04 23:18:53 +0100

    routes bugs fixed

--- client/src/App.js
+++ client/src/App.js
@@ -16,9 +16,9 @@ function App() {
     <BrowserRouter>
           <Switch>
           <Route exact path='/' component={Home}/>
-          <Route  path='/resume' component={Resume}/>
-          <Route  path='/portfolio' component={Portfolio}/>
-          <Route  path='/contact' component={Contact}/>
+          <Route exact path='/resume' component={Resume}/>
+          <Route exact path='/portfolio' component={Portfolio}/>
+          <Route exact path='/contact' component={Contact}/>
 
           </Switch>
     </BrowserRouter>
